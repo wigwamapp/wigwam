f4d8e8185906cd8c38cc980dffe96b654fcc200f
ogdeer <ogdeer@protonmail.com>
2021-02-18 15:54:58 +0200

    Improve content script

--- src/content-script.ts
+++ src/content-script.ts
@@ -1,3 +1,11 @@
 declare var chrome: any;
 
-import(chrome.extension.getURL("core/content-main.js")).catch(console.error);
+// Content script is injecting a script with ES6 module support into the current page
+const script = document.createElement("script");
+script.setAttribute("type", "module");
+script.setAttribute("src", chrome.extension.getURL("core/content-main.js"));
+const head =
+  document.head ||
+  document.getElementsByTagName("head")[0] ||
+  document.documentElement;
+head.insertBefore(script, head.lastChild);
