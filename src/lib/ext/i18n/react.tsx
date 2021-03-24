3a64d8dfc789e50f2a1b1242f439001e2401726b
ogdeer <ogdeer@protonmail.com>
2021-03-24 22:37:31 +0200

    Add stub for i18n

---
+++ src/lib/ext/i18n/react.tsx
@@ -0,0 +1,13 @@
+import React, { memo } from "react";
+
+export type TProps = {
+  id?: string;
+};
+
+export const T = memo<TProps>(({ children }) => {
+  return <>{children}</>;
+});
+
+export const t = (msg: string) => msg;
+
+// TODO: Implement well i18n
