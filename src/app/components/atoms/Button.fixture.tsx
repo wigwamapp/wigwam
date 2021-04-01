bb789496a53bbefd73771b61477efea0270a02a1
ogdeer <ogdeer@protonmail.com>
2021-04-02 00:59:15 +0300

    Add example Button Component

---
+++ src/app/components/atoms/Button.fixture.tsx
@@ -0,0 +1,4 @@
+import React from "react";
+import Button from "./Button";
+
+export default <Button>Hello</Button>;
---
+++ src/app/components/atoms/Button.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import classNames from "clsx";
+
+type ButtonProps = React.HTMLAttributes<HTMLButtonElement>;
+
+const Button: React.FC<ButtonProps> = ({ className, ...rest }) => (
+  <button
+    className={classNames(
+      "bg-blue-500 hover:bg-blue-700",
+      "border border-blue-700",
+      "rounded",
+      "py-2 px-4",
+      "text-white font-bold",
+      className
+    )}
+    {...rest}
+  />
+);
+
+export default Button;
