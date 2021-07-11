84fcf3c2b8dd8e4997c25ceebd14353eea890b6e
ogdeer <ogdeer@protonmail.com>
2021-07-11 17:54:02 +0300

    Fix terser plugin

--- webpack.config.js
+++ webpack.config.js
@@ -384,7 +384,7 @@ module.exports = {
             ecma: 8,
           },
           compress: {
-            ecma: 5,
+            ecma: 8,
             warnings: false,
             // Disabled because of an issue with Uglify breaking seemingly valid code:
             // https://github.com/facebook/create-react-app/issues/2376
@@ -399,11 +399,8 @@ module.exports = {
             // Drop console for prod
             drop_console: NODE_ENV === "production",
           },
-          mangle: {
-            safari10: true,
-          },
           output: {
-            ecma: 5,
+            ecma: 8,
             comments: false,
             // Turned on because emoji and regex is not minified properly using default
             // https://github.com/facebook/create-react-app/issues/2488
