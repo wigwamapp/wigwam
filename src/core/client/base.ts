bac0569fd3763b475ef14ce6d5a35ce102a3b3f3
ogdeer <ogdeer@protonmail.com>
2021-11-11 21:12:00 +0200

    Restructure core/client

--- src/core/client/base.ts
+++ src/core/client/base.ts
@@ -1,38 +1,6 @@
 import { JsonRpcProvider } from "@ethersproject/providers";
-import memoizeOne from "memoize-one";
 import { PorterClient } from "lib/ext/porter/client";
-import { assert } from "lib/system/assert";
 
-import { MessageType, Request, Response, RpcResponse } from "core/types";
+import { Request, Response } from "core/types";
 
 export const porter = new PorterClient<Request, Response>();
-
-export class ClientProvider extends JsonRpcProvider {
-  constructor(chainId: number) {
-    super("", chainId);
-  }
-
-  getNetwork = memoizeOne(super.getNetwork.bind(this));
-
-  async send(method: string, params: Array<any>): Promise<any> {
-    const type = MessageType.SendRpc;
-    const { chainId } = this.network;
-
-    const res = await porter.request({ type, chainId, method, params });
-    assert(res?.type === type);
-
-    return getResult(res.response);
-  }
-}
-
-function getResult(response: RpcResponse): any {
-  if ("error" in response) {
-    const error = new Error(response.error.message);
-    (error as any).code = response.error.code;
-    (error as any).data = response.error.data;
-
-    throw error;
-  }
-
-  return response.result;
-}
---
+++ src/core/client/provider.ts
@@ -0,0 +1,37 @@
+import { JsonRpcProvider } from "@ethersproject/providers";
+import memoizeOne from "memoize-one";
+import { assert } from "lib/system/assert";
+
+import { MessageType, RpcResponse } from "core/types";
+
+import { porter } from "./base";
+
+export class ClientProvider extends JsonRpcProvider {
+  constructor(chainId: number) {
+    super("", chainId);
+  }
+
+  getNetwork = memoizeOne(super.getNetwork.bind(this));
+
+  async send(method: string, params: Array<any>): Promise<any> {
+    const type = MessageType.SendRpc;
+    const { chainId } = this.network;
+
+    const res = await porter.request({ type, chainId, method, params });
+    assert(res?.type === type);
+
+    return getResult(res.response);
+  }
+}
+
+function getResult(response: RpcResponse): any {
+  if ("error" in response) {
+    const error = new Error(response.error.message);
+    (error as any).code = response.error.code;
+    (error as any).data = response.error.data;
+
+    throw error;
+  }
+
+  return response.result;
+}
