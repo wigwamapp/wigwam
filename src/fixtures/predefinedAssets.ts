d42e6a00eb3e15a6708127c158bf8831b23b41a6
ogdeer <ogdeer@protonmail.com>
2021-11-11 13:19:30 +0200

    Add predefinedAssets fixtures

---
+++ src/fixtures/predefinedAssets.ts
@@ -0,0 +1,12 @@
+export const PREDEFINED_ERC20_MAP = new Map<number, string[]>([
+  [
+    1, // Ethereum
+    [
+      "0xdac17f958d2ee523a2206206994597c13d831ec7", // USDT
+      "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48", // USDC
+      "0x1f9840a85d5af5bf1d1762f925bdaddc4201f984", // UNI
+      "0x2260fac5e5542a773aa44fbcfedf7c193bc2c599", // WBTC
+      "0x6b175474e89094c44da98b954eedeac495271d0f", // DAI
+    ],
+  ],
+]);
