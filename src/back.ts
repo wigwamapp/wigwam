7b648b841a58a56e01662f314f0b24c6a967878a
ogdeer <ogdeer@protonmail.com>
2021-11-23 15:50:42 +0200

    Remove comments

--- src/back.ts
+++ src/back.ts
@@ -34,36 +34,3 @@ browser.runtime.onInstalled.addListener(({ reason }) => {
       .catch(() => undefined);
   }
 });
-
-// for (const _i of [1, 2, 3, 4, 5, 6, 7, 8]) {
-//   new Worker(new URL("./worker", import.meta.url));
-// }
-
-// setTimeout(async () => {
-//   const [currentTab] = await browser.tabs.query({
-//     currentWindow: true,
-//     active: true,
-//   });
-
-//   const tab = await browser.tabs.create({
-//     windowId: currentTab.windowId,
-//     index: currentTab.index + 1,
-//     url: browser.runtime.getURL("main.html"),
-//     active: true,
-//     openerTabId: currentTab.id,
-//   });
-
-//   setTimeout(() => {
-//     browser.tabs.remove(tab.id!);
-//   }, 5_000);
-
-//   // await browser.windows.create({
-//   //   type: "popup",
-//   //   tabId: currentTab.id,
-//   //   focused: true,
-//   //   width: 500,
-//   //   height: 500,
-//   //   top: 20,
-//   //   left: 20,
-//   // });
-// }, 5_000);
