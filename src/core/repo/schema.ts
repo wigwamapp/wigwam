4a205dfed5548efcdaf07287e3526c7b0496df18
ogdeer <ogdeer@protonmail.com>
2021-12-07 23:15:02 +0200

    Add experiment with async init for Dexie instance

--- src/core/repo/schema.ts
+++ src/core/repo/schema.ts
@@ -4,7 +4,25 @@ import { underProfile } from "lib/ext/profile";
 
 import { RepoTable } from "./types";
 
-export const db = new Dexie(underProfile("main"));
+class MyDexie extends Dexie {
+  constructor(private namePromise: Promise<string>) {
+    super("<stub>");
+  }
+
+  open() {
+    return new Dexie.Promise<Dexie>((res, rej) =>
+      this.namePromise
+        .then((name) => {
+          Object.assign(this, { name });
+        })
+        .then(() => super.open())
+        .then(res)
+        .catch(rej)
+    );
+  }
+}
+
+export const db = new MyDexie(Promise.resolve(underProfile("main")));
 
 /**
  * 1
@@ -14,3 +32,16 @@ db.version(1).stores({
   [RepoTable.Networks]: "&chainId,type,chainTag",
   [RepoTable.Accounts]: "&address,type,source",
 });
+
+db.version(2)
+  .stores({
+    [RepoTable.Accounts]: "&address,type",
+  })
+  .upgrade(async (tx) => {
+    await tx
+      .table(RepoTable.Accounts)
+      .toCollection()
+      .modify((acc) => {
+        delete acc.source;
+      });
+  });
