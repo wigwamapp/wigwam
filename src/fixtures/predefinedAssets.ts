f8ee1348c284040d194f17f7fd5c582cb4044e86
ogdeer <ogdeer@protonmail.com>
2021-12-08 20:16:06 +0200

    Add more predefined ERC20 tokens

--- src/fixtures/predefinedAssets.ts
+++ src/fixtures/predefinedAssets.ts
@@ -7,6 +7,8 @@ export const PREDEFINED_ERC20_MAP = new Map<number, string[]>([
       "0x1f9840a85d5af5bf1d1762f925bdaddc4201f984", // UNI
       "0x2260fac5e5542a773aa44fbcfedf7c193bc2c599", // WBTC
       "0x6b175474e89094c44da98b954eedeac495271d0f", // DAI
+      "0x514910771af9ca656af840dff83e8264ecf986ca", // LINK
+      "0x0f5d2fb29fb7d3cfee444a200298f468908cc942", // MANA
     ],
   ],
 ]);
