a5c38820331aac1ff97d907f8eb3afe66c85a426
pas1ko <git@serh11p.com>
2022-01-15 10:45:15 +0200

    Set argon2 params more strict

--- src/lib/kdbx/index.ts
+++ src/lib/kdbx/index.ts
@@ -3,15 +3,14 @@ import * as Argon2 from "lib/argon2";
 
 export * from "kdbxweb";
 
-CryptoEngine.setArgon2Impl(
-  (pass, salt, mem, time, hashLen, parallelism, type) =>
-    Argon2.hash({
-      pass: new Uint8Array(pass),
-      salt: new Uint8Array(salt),
-      mem,
-      time,
-      hashLen,
-      parallelism,
-      type,
-    }).then(({ hash }) => hash)
+CryptoEngine.setArgon2Impl((pass, salt, _mem, _time, hashLen, parallelism) =>
+  Argon2.hash({
+    pass: new Uint8Array(pass),
+    salt: new Uint8Array(salt),
+    mem: 1024 * 15, // 15 MiB
+    time: 2,
+    hashLen,
+    parallelism,
+    type: 2, // Argon2id
+  }).then(({ hash }) => hash)
 );
