e2f4d5ab9d2e40c0e657a8e345cbb1c06fb7f2d9
pas1ko <git@serh11p.com>
2022-02-05 21:53:59 +0200

    Implement useOnScreen

---
+++ src/lib/react-hooks/useOnScreen.ts
@@ -0,0 +1,31 @@
+import { RefObject, useState, useEffect, useMemo } from "react";
+
+export function useOnScreen<T extends Element>(
+  ref: RefObject<T>,
+  rootMargin = "0px"
+) {
+  // State and setter for storing whether element is visible
+  const [isIntersecting, setIntersecting] = useState(false);
+
+  const observer = useMemo(
+    () =>
+      new IntersectionObserver(
+        ([entry]) => {
+          // Update our state when observer callback fires
+          setIntersecting(entry.isIntersecting);
+        },
+        { rootMargin }
+      ),
+    [setIntersecting, rootMargin]
+  );
+
+  useEffect(() => {
+    const target = ref.current;
+    if (!target) return;
+
+    observer.observe(target);
+    return () => observer.unobserve(target);
+  }, [ref, observer]);
+
+  return isIntersecting;
+}
