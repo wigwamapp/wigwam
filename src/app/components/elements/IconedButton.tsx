c1bd0940d0c80b1cc0089cb2f1de16b072db69e0
pas1ko <git@serh11p.com>
2022-02-19 22:21:02 +0000

    Fix react warning

--- src/app/components/elements/IconedButton.tsx
+++ src/app/components/elements/IconedButton.tsx
@@ -1,74 +1,80 @@
-import { FC, HTMLAttributes } from "react";
+import { FC, forwardRef, HTMLAttributes, ForwardedRef } from "react";
 import classNames from "clsx";
 import Link, { LinkProps } from "lib/navigation/Link";
 
-type NewButtonProps = {
+type IconedButtonProps = {
   Icon: FC<{ className?: string }>;
   theme?: "primary" | "secondary" | "tertiary";
   iconClassName?: string;
 } & (HTMLAttributes<HTMLButtonElement> | LinkProps);
 
-const NewButton: FC<NewButtonProps> = ({
-  theme = "primary",
-  className,
-  Icon,
-  iconClassName,
-  ...rest
-}) => {
-  const classNamesList = classNames(
-    "group",
-    (theme === "primary" || theme === "secondary") && "w-5 h-5",
-    theme === "tertiary" && "w-6 h-6",
-    theme === "primary" && "bg-brand-main/20",
-    theme === "secondary" && "bg-brand-main/[.05]",
-    "rounded",
-    "flex justify-center items-center",
-    "transition",
-    "hover:bg-brand-main/30 hover:shadow-buttonsecondary",
-    "focus:bg-brand-main/30 focus:shadow-buttonsecondary",
-    "active:bg-brand-main/20 active:shadow-none",
-    className
-  );
+const IconedButton = forwardRef<HTMLElement, IconedButtonProps>(
+  ({ theme = "primary", className, Icon, iconClassName, ...rest }, ref) => {
+    const classNamesList = classNames(
+      "group",
+      (theme === "primary" || theme === "secondary") && "w-5 h-5",
+      theme === "tertiary" && "w-6 h-6",
+      theme === "primary" && "bg-brand-main/20",
+      theme === "secondary" && "bg-brand-main/[.05]",
+      "rounded",
+      "flex justify-center items-center",
+      "transition",
+      "hover:bg-brand-main/30 hover:shadow-buttonsecondary",
+      "focus:bg-brand-main/30 focus:shadow-buttonsecondary",
+      "active:bg-brand-main/20 active:shadow-none",
+      className
+    );
 
-  const content = (
-    <Icon
-      className={classNames(
-        (theme === "primary" || theme === "secondary") && "w-4",
-        theme === "tertiary" && "w-6",
-        "h-auto",
-        "transition-opacity",
-        "group-active:opacity-60",
-        iconClassName
-      )}
-    />
-  );
+    const content = (
+      <Icon
+        className={classNames(
+          (theme === "primary" || theme === "secondary") && "w-4",
+          theme === "tertiary" && "w-6",
+          "h-auto",
+          "transition-opacity",
+          "group-active:opacity-60",
+          iconClassName
+        )}
+      />
+    );
 
-  if ("href" in rest) {
+    if ("href" in rest) {
+      return (
+        <a
+          ref={ref as ForwardedRef<HTMLAnchorElement>}
+          target="_blank"
+          rel="nofollow noreferrer"
+          className={classNamesList}
+          {...rest}
+        >
+          {content}
+        </a>
+      );
+    }
+
+    if ("to" in rest) {
+      return (
+        <Link
+          ref={ref as ForwardedRef<HTMLAnchorElement>}
+          className={classNamesList}
+          {...rest}
+        >
+          {content}
+        </Link>
+      );
+    }
+
     return (
-      <a
-        target="_blank"
-        rel="nofollow noreferrer"
+      <button
+        ref={ref as ForwardedRef<HTMLButtonElement>}
+        type="button"
         className={classNamesList}
         {...rest}
       >
         {content}
-      </a>
+      </button>
     );
   }
+);
 
-  if ("to" in rest) {
-    return (
-      <Link className={classNamesList} {...rest}>
-        {content}
-      </Link>
-    );
-  }
-
-  return (
-    <button type="button" className={classNamesList} {...rest}>
-      {content}
-    </button>
-  );
-};
-
-export default NewButton;
+export default IconedButton;
