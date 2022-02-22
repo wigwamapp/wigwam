621fd10b4d4b287aa5aaea1bb9d2a49311b52759
Valentyn Shapoval <valentin.vad.sh@gmail.com>
2022-02-22 20:28:17 +0200

    tooltip last before blur

--- src/app/components/elements/Tooltip.tsx
+++ src/app/components/elements/Tooltip.tsx
@@ -16,7 +16,7 @@ const config = {
     align: "start" as const,
     alignOffset: -10,
     paddings: "py-5 px-5",
-    fill: "#1e2132",
+    fill: "fill-brand-darklight/30",
   },
   secondary: {
     side: "bottom" as const,
@@ -24,7 +24,7 @@ const config = {
     align: "center" as const,
     alignOffset: 0,
     paddings: "py-3 px-8",
-    fill: "#33364b",
+    fill: "fill-[#33364b]",
   },
 };
 const Tooltip: FC<TooltipProps> = ({
@@ -41,31 +41,32 @@ const Tooltip: FC<TooltipProps> = ({
       <TooltipPrimitive.TooltipTrigger asChild={asChild} className={className}>
         {children}
       </TooltipPrimitive.TooltipTrigger>
-      <TooltipPrimitive.Content
-        side={config[theme].side}
-        portalled
-        sideOffset={config[theme].sideOffset}
-        align={config[theme].align}
-        alignOffset={config[theme].alignOffset}
-        className={classNames(
-          "rounded-[.625rem]",
-          theme === "secondary" && "bg-[#33364b]",
-          theme === "primary" &&
-            "w-[18rem] bg-brand-darklight/30 backdrop-filter backdrop-blur-lg",
-          "border border-brand-light/5",
-          config[theme].paddings,
-          "text-white",
-          "z-10"
-        )}
-      >
-        {content}
-        <TooltipPrimitive.Arrow
-          offset={13}
-          width={15}
-          height={8}
-          className={`stroke-[#323453] fill-[${config[theme].fill}]`}
-        />
-      </TooltipPrimitive.Content>
+      <div className="bg-brand-darklight/30 backdrop-blur-lg">
+        <TooltipPrimitive.Content
+          portalled
+          side={config[theme].side}
+          sideOffset={config[theme].sideOffset}
+          align={config[theme].align}
+          alignOffset={config[theme].alignOffset}
+          className={classNames(
+            "rounded-[.625rem]",
+            theme === "secondary" && "bg-[#33364b]",
+            theme === "primary" &&
+              "w-[18rem] bg-brand-darklight/30 backdrop-blur-md",
+            config[theme].paddings,
+            "text-white",
+            "z-10"
+          )}
+        >
+          {content}
+          <TooltipPrimitive.Arrow
+            offset={13}
+            width={15}
+            height={8}
+            className={config[theme].fill}
+          />
+        </TooltipPrimitive.Content>
+      </div>
     </TooltipPrimitive.Root>
   );
 };
