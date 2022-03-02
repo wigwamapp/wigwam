14f8e7008385dc688113a04e6e5844504045f7b5
olehkhalin <olehkhalin@gmail.com>
2022-03-02 13:26:41 -0500

    Some more experiments

--- src/app/components/elements/NewTooltip.tsx
+++ src/app/components/elements/NewTooltip.tsx
@@ -1,4 +1,12 @@
-import { FC, ReactNode, useCallback, useEffect, useRef, useState } from "react";
+import {
+  FC,
+  ReactNode,
+  useCallback,
+  useEffect,
+  useMemo,
+  useRef,
+  useState,
+} from "react";
 import {
   useFloating,
   shift,
@@ -6,14 +14,16 @@ import {
   autoPlacement,
   hide,
   arrow,
+  autoUpdate,
 } from "@floating-ui/react-dom";
 import classNames from "clsx";
 import * as Portal from "@radix-ui/react-portal";
+import { match } from "ts-pattern";
 
-type sizeType = "large" | "small";
+type SizeType = "large" | "small";
 
 type NewTooltip = {
-  size?: sizeType;
+  size?: SizeType;
   content: ReactNode;
   className?: string;
 };
@@ -24,30 +34,32 @@ const NewTooltip: FC<NewTooltip> = ({
   className,
   children,
 }) => {
-  const [open, setOpen] = useState(false);
+  const [open, setOpen] = useState(true);
   const arrowRef = useRef(null);
   const openTimerRef = useRef(0);
   const closeTimerRef = useRef(0);
 
   const {
+    refs,
     x,
     y,
     reference,
+    placement,
     floating,
     strategy,
     update,
     middlewareData: { arrow: { x: arrowX, y: arrowY } = {} },
   } = useFloating({
-    // placement: "left-start",
+    placement: "top",
     middleware: [
-      shift(),
       offset(10),
-      autoPlacement({
-        allowedPlacements:
-          size === "large" ? ["right", "left"] : ["top", "bottom"],
-      }),
-      hide(),
+      // autoPlacement({
+      //   allowedPlacements:
+      //     size === "large" ? ["right", "left"] : ["top", "bottom"],
+      // }),
+      shift(),
       arrow({ element: arrowRef }),
+      hide(),
     ],
   });
 
@@ -74,6 +86,48 @@ const NewTooltip: FC<NewTooltip> = ({
       clearTimeout(closeTimerRef.current);
     };
   }, []);
+
+  useEffect(() => {
+    if (!refs.reference.current || !refs.floating.current) {
+      return;
+    }
+
+    return autoUpdate(refs.reference.current, refs.floating.current, update);
+  }, [refs.reference, refs.floating, update]);
+
+  const arrowInfo = useMemo(() => {
+    if (!refs.floating.current) {
+      return null;
+    }
+    const arrowDirection = getArrowDirection(placement);
+
+    if (!arrowDirection) {
+      return null;
+    }
+
+    return match(arrowDirection)
+      .with("toLeft", () => ({
+        y: arrowY,
+        x: -getArrowDimensions(size).width,
+        direction: arrowDirection,
+      }))
+      .with("toRight", () => ({
+        y: arrowY,
+        x: "100%",
+        direction: arrowDirection,
+      }))
+      .with("toTop", () => ({
+        y: -getArrowDimensions(size).height,
+        x: arrowX,
+        direction: arrowDirection,
+      }))
+      .with("toBottom", () => ({
+        y: "100%",
+        x: arrowX,
+        direction: arrowDirection,
+      }))
+      .otherwise(() => null);
+  }, [arrowX, arrowY, placement, refs.floating, size]);
 
   return (
     <>
@@ -107,14 +161,19 @@ const NewTooltip: FC<NewTooltip> = ({
           onBlur={handleClose}
         >
           {content}
-          <div
-            ref={arrowRef}
-            className="absolute w-2 h-2 bg-white rotate-45"
-            style={{
-              top: arrowY ?? "",
-              left: arrowX ?? "",
-            }}
-          />
+          {arrowInfo && (
+            <div
+              ref={arrowRef}
+              className={classNames(
+                "absolute w-0 h-0",
+                getArrowClassNames(size, arrowInfo.direction)
+              )}
+              style={{
+                top: arrowInfo.y,
+                left: arrowInfo.x,
+              }}
+            />
+          )}
         </div>
       </Portal.Root>
     </>
@@ -123,19 +182,85 @@ const NewTooltip: FC<NewTooltip> = ({
 
 export default NewTooltip;
 
-const getSizeClasses = (size: sizeType) => {
-  if (size === "large") {
-    return classNames(
-      "rounded-[.625rem]",
-      "w-max-content max-w-[18rem]",
-      "bg-brand-main/10 backdrop-blur-[60px]",
-      "py-5 px-5"
+const getSizeClasses = (size: SizeType) =>
+  match(size)
+    .with("large", () =>
+      classNames(
+        "rounded-[.625rem]",
+        "w-max-content max-w-[18rem]",
+        "bg-brand-main/10 backdrop-blur-[60px]",
+        "py-5 px-5"
+      )
+    )
+    .otherwise(() =>
+      classNames(
+        "rounded-md",
+        "bg-brand-main/20 backdrop-blur-[6px]",
+        "py-1.5 px-3"
+      )
     );
-  }
 
-  return classNames(
-    "rounded-md",
-    "bg-brand-main/20 backdrop-blur-[6px]",
-    "py-1.5 px-3"
-  );
+const getArrowDirection = (placement: string) => {
+  if (placement.startsWith("left")) {
+    return "toRight";
+  }
+  if (placement.startsWith("right")) {
+    return "toLeft";
+  }
+  if (placement.startsWith("top")) {
+    return "toBottom";
+  }
+  if (placement.startsWith("bottom")) {
+    return "toTop";
+  }
+  return null;
 };
+
+const getArrowDimensions = (size: SizeType) =>
+  match(size)
+    .with("large", () => ({
+      width: 6,
+      height: 20,
+    }))
+    .otherwise(() => ({
+      width: 3,
+      height: 7,
+    }));
+
+const getArrowClassNames = (size: SizeType, direction: string) =>
+  match(size)
+    .with("large", () => {
+      const color = classNames("border-brand-main/10 backdrop-blur-[60px]");
+
+      return match(direction)
+        .with("toTop", () =>
+          classNames(
+            color,
+            "border-x-[10px] border-t-[6px] border-x-transparent"
+          )
+        )
+        .with("toBottom", () =>
+          classNames(
+            color,
+            "border-x-[10px] border-b-[6px] border-x-transparent"
+          )
+        )
+        .with("toRight", () =>
+          classNames(
+            color,
+            "border-y-[10px] border-l-[6px] border-y-transparent"
+          )
+        )
+        .with("toLeft", () =>
+          classNames(
+            color,
+            "border-y-[10px] border-r-[6px] border-y-transparent"
+          )
+        );
+    })
+    .otherwise(() =>
+      classNames(
+        "w-0 h-0 border-x-[7px] border-b-[3px] border-x-transparent" +
+          " border-b-[#000000]"
+      )
+    );
