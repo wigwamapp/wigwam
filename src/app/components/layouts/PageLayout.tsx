ffa6270b156440271becc03fb397fdab5a07714f
pas1ko <git@serh11p.com>
2022-03-05 01:23:41 +0000

    Fix popup blinking

--- src/app/components/layouts/PageLayout.tsx
+++ src/app/components/layouts/PageLayout.tsx
@@ -1,13 +1,12 @@
 import { FC } from "react";
 import classNames from "clsx";
-import { useAtomValue } from "jotai";
-import { waitForAll } from "jotai/utils";
 
-import { chainIdAtom, currentAccountAtom } from "app/atoms";
 import ContentContainer from "app/components/layouts/ContentContainer";
 import Sidebar from "app/components/blocks/Sidebar";
 import Menu from "app/components/blocks/Menu";
 import ActivityBar from "app/components/blocks/ActivityBar";
+
+import PreloadUnlocked from "./PreloadUnlocked";
 
 let bootAnimationDisplayed = true;
 const handleBootAnimationEnd = () => {
@@ -49,9 +48,3 @@ const PageLayout: FC<{ animate?: boolean; className?: string }> = ({
 );
 
 export default PageLayout;
-
-const PreloadUnlocked: FC = ({ children }) => {
-  useAtomValue(waitForAll([currentAccountAtom, chainIdAtom]));
-
-  return <>{children}</>;
-};
