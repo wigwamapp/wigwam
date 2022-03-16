4889b1baaf04409c70e76fa1e74ab10d50d45ad3
Valentyn Shapoval <valentin.vad.sh@gmail.com>
2022-03-16 22:02:49 +0200

    password form block carried out

--- src/app/components/pages/Unlock.tsx
+++ src/app/components/pages/Unlock.tsx
@@ -1,33 +1,20 @@
-import {
-  FC,
-  memo,
-  useRef,
-  useCallback,
-  FormEventHandler,
-  useState,
-} from "react";
+import { FC, memo, useCallback } from "react";
 import classNames from "clsx";
 import { useAtomValue } from "jotai";
 import { isPopup as isPopupPrimitive } from "lib/ext/view";
-
-import { unlockWallet } from "core/client";
 
 import { Page } from "app/defaults";
 import { currentProfileAtom } from "app/atoms";
 import { openInTab } from "app/helpers";
 import BoardingPageLayout from "app/components/layouts/BoardingPageLayout";
 import PopupLayout from "app/components/layouts/PopupLayout";
-import Input from "app/components/elements/Input";
-import NewButton from "app/components/elements/NewButton";
-import IconedButton from "app/components/elements/IconedButton";
 import SecondaryModal, {
   SecondaryModalProps,
 } from "app/components/elements/SecondaryModal";
 import ProfilePreview from "app/components/blocks/ProfilePreview";
-import { ReactComponent as EyeIcon } from "app/icons/eye.svg";
-import { ReactComponent as OpenedEyeIcon } from "app/icons/opened-eye.svg";
 import { ReactComponent as ChangeProfileIcon } from "app/icons/change-profile.svg";
 import { ReactComponent as VigvamIcon } from "app/icons/Vigvam.svg";
+import PasswordForm from "../blocks/PasswordForm";
 
 const Unlock: FC = () => {
   const currentProfile = useAtomValue(currentProfileAtom);
@@ -51,7 +38,7 @@ const Unlock: FC = () => {
           />
         </div>
       </div>
-      <UnlockForm
+      <PasswordForm
         theme={isPopup ? "small" : "large"}
         className={isPopup ? "mt-11" : "mt-12"}
       />
@@ -78,95 +65,6 @@ const Unlock: FC = () => {
 };
 
 export default Unlock;
-
-type UnlockFormProps = {
-  theme?: "large" | "small";
-  unlockCallback?: (password: string) => void;
-  className?: string;
-};
-
-export const UnlockForm = memo<UnlockFormProps>(
-  ({ theme = "large", unlockCallback, className }) => {
-    const passwordFieldRef = useRef<HTMLInputElement>(null);
-    const [inputShowContent, setInputShowContent] = useState(false);
-    const [attention, setAttention] = useState(false);
-
-    const handleSubmit = useCallback<FormEventHandler<HTMLFormElement>>(
-      async (evt) => {
-        evt.preventDefault();
-
-        const password = passwordFieldRef.current?.value;
-        if (password) {
-          try {
-            if (unlockCallback) {
-              unlockCallback(password);
-            } else {
-              await unlockWallet(password);
-            }
-          } catch (err: any) {
-            alert(err?.message);
-          }
-        }
-      },
-      [unlockCallback]
-    );
-
-    return (
-      <form
-        className={classNames("w-full flex flex-col items-center", className)}
-        onSubmit={handleSubmit}
-      >
-        <div className="max-w-[19rem] w-full relative">
-          <Input
-            ref={passwordFieldRef}
-            type={inputShowContent ? "text" : "password"}
-            placeholder="Type password"
-            label="Password"
-            className="w-full"
-          />
-          <IconedButton
-            Icon={inputShowContent ? EyeIcon : OpenedEyeIcon}
-            aria-label={`${inputShowContent ? "Hide" : "Show"} password`}
-            theme="tertiary"
-            onClick={() => setInputShowContent(!inputShowContent)}
-            tabIndex={-1}
-            className="absolute bottom-2.5 right-3"
-          />
-        </div>
-
-        <div
-          className={classNames(
-            "max-w-[13.75rem] w-full center",
-            theme === "large" && "mt-6",
-            theme === "small" && "mt-4"
-          )}
-        >
-          <NewButton type="submit" className="w-full">
-            Unlock
-          </NewButton>
-          <button
-            className={classNames(
-              "w-full text-brand-inactivelight",
-              theme === "large" && "text-sm mt-4",
-              theme === "small" && "text-xs mt-3"
-            )}
-            onClick={() => {
-              setAttention(true);
-            }}
-          >
-            <u>Forgot the password?</u>
-            <br />
-            Want to <u>sign into another profile?</u>
-          </button>
-        </div>
-        <AttentionModal
-          open={attention}
-          onOpenChange={() => setAttention(false)}
-        />
-      </form>
-    );
-  }
-);
 
 type ChangeProfileButtonProps = {
   theme?: "large" | "small";
@@ -225,28 +123,31 @@ const ChangeProfileButton = memo<ChangeProfileButtonProps>(
   }
 );
 
-const AttentionModal = memo<SecondaryModalProps>(({ open, onOpenChange }) => {
-  return (
-    <SecondaryModal
-      open={open}
-      onOpenChange={onOpenChange}
-      className="px-[5.25rem]"
-    >
-      <ul>
-        <ListBlock className="mb-5">You cannot reset Application.</ListBlock>
-        <ListBlock>
-          To restore with Seed Phrase, or if you want to start from scratch, go
-          to Profiles and Add a New Profile!
-        </ListBlock>
-      </ul>
-      <p className="text-base text-brand-inactivelight mt-5">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam,
-        purus sit amet luctus venenatis, lectus magna fringilla urna, porttitor
-        rhoncus dolor purus non enim praesent elementum facilisis leo
-      </p>
-    </SecondaryModal>
-  );
-});
+export const AttentionModal = memo<SecondaryModalProps>(
+  ({ open, onOpenChange }) => {
+    return (
+      <SecondaryModal
+        open={open}
+        onOpenChange={onOpenChange}
+        className="px-[5.25rem]"
+      >
+        <ul>
+          <ListBlock className="mb-5">You cannot reset Application.</ListBlock>
+          <ListBlock>
+            To restore with Seed Phrase, or if you want to start from scratch,
+            go to Profiles and Add a New Profile!
+          </ListBlock>
+        </ul>
+        <p className="text-base text-brand-inactivelight mt-5">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam,
+          purus sit amet luctus venenatis, lectus magna fringilla urna,
+          porttitor rhoncus dolor purus non enim praesent elementum facilisis
+          leo
+        </p>
+      </SecondaryModal>
+    );
+  }
+);
 
 type ListBlockProps = {
   className?: string;
