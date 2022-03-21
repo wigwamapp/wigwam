caee230e622e5d7a9e1dba0b6cc48de738dfa731
Valentyn Shapoval <valentin.vad.sh@gmail.com>
2022-03-21 20:40:12 +0200

    rems, settingsHeader and links array

--- src/app/components/elements/SettingsHeader.tsx
+++ src/app/components/elements/SettingsHeader.tsx
@@ -1,17 +1,7 @@
 import { FC } from "react";
-import classNames from "clsx";
 
-type SettingsHeaderProps = {
-  className?: string;
-};
-
-const SettingsHeader: FC<SettingsHeaderProps> = ({
-  className = "",
-  children,
-}) => (
-  <h3 className={classNames("font-bold text-2xl leading-none mb-6", className)}>
-    {children}
-  </h3>
+const SettingsHeader: FC = ({ children }) => (
+  <h3 className={"font-bold text-2xl leading-none mb-6"}>{children}</h3>
 );
 
 export default SettingsHeader;
--- src/app/components/screens/settingTabs/About.tsx
+++ src/app/components/screens/settingTabs/About.tsx
@@ -1,35 +1,62 @@
 import { FC } from "react";
 import classNames from "clsx";
 
-import SettingsHeader from "app/components/elements/SettingsHeader";
 import { ReactComponent as VigvamLogo } from "app/icons/Vigvam.svg";
 
 const About: FC = () => {
   return (
     <div className={classNames("flex flex-col", "py-3 px-4 mb-2")}>
-      <SettingsHeader className="!text-base !mb-4">
-        Vigvam version
-      </SettingsHeader>
+      <h3 className="font-bold leading-none text-base mb-4">Vigvam version</h3>
       <div className="text-brand-font font-bold text-xl">10.4.1</div>
-      <p className="text-brand-font text-sm mt-[35px] mb-[33px]">
+      <p className="text-brand-font text-sm mt-[2.188rem] mb-[2.063rem]">
         Vigvam lets you to explore DeFi and NFTs in safer, faster and modern
         way.
       </p>
-      <SettingsHeader className="!text-base !mb-0.5">Links</SettingsHeader>
-      <Link label="Privacy policy" href="google.com" />
-      <Link label="Terms of Use" href="google.com" />
-      <Link label="Attributions" href="google.com" />
-      <div className="h-px w-[71px] my-2 bg-brand-main/10"></div>
-      <Link label="Visit our Support Center" href="google.com" />
-      <Link label="Visit our web site" href="google.com" />
-      <Link label="Contact us" href="google.com" />
+      <h3 className="font-bold leading-none text-base mb-4">Links</h3>
+      {linksBefore.map((props) => (
+        <Link key={props.label} {...props} />
+      ))}
+      <div className="h-px w-[4.438rem] my-2 bg-brand-main/10"></div>
+      {linksAfter.map((props) => (
+        <Link key={props.label} {...props} />
+      ))}
       <div className="flex items-center mt-12">
-        <VigvamLogo className="w-[52px] h-8" />
+        <VigvamLogo className="w-[3.25rem] h-8" />
         <h1 className="text-2xl font-black ml-4">Vigvam</h1>
       </div>
     </div>
   );
 };
+
+const linksBefore = [
+  {
+    label: "Privacy policy",
+    href: "https://google.com",
+  },
+  {
+    label: "Terms of Use",
+    href: "https://google.com",
+  },
+  {
+    label: "Attributions",
+    href: "https://google.com",
+  },
+];
+
+const linksAfter = [
+  {
+    label: "Visit our Support Center",
+    href: "https://google.com",
+  },
+  {
+    label: "Visit our web site",
+    href: "https://google.com",
+  },
+  {
+    label: "Contact us",
+    href: "https://google.com",
+  },
+];
 
 type LinkProps = {
   label: string;
