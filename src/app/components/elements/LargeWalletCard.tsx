baa38b55254276daaa3a1be8ea5a3825e6dfe27c
pas1ko <git@serh11p.com>
2022-03-31 13:14:26 +0100

    Add experimental transition for large wallet card

--- src/app/components/elements/LargeWalletCard.tsx
+++ src/app/components/elements/LargeWalletCard.tsx
@@ -1,7 +1,8 @@
-import { FC, useState } from "react";
+import { memo, useRef, useState } from "react";
 import classNames from "clsx";
 import { ethers } from "ethers";
 import BigNumber from "bignumber.js";
+import { CSSTransition, SwitchTransition } from "react-transition-group";
 import { TReplace } from "lib/ext/i18n/react";
 
 import { Account } from "core/types";
@@ -30,164 +31,187 @@ type LargeWalletCardProps = {
   className?: string;
 };
 
-const LargeWalletCard: FC<LargeWalletCardProps> = ({
-  account: { name, address },
-  className,
-}) => {
-  const [copied, setCopied] = useState(false);
+const LargeWalletCard = memo<LargeWalletCardProps>(
+  ({ account: { name, address }, className }) => {
+    const [copied, setCopied] = useState(false);
 
-  const currentNetwork = useLazyNetwork();
-  const nativeToken = useAccountNativeToken(address);
-  const portfolioBalance = nativeToken?.portfolioUSD;
+    const currentNetwork = useLazyNetwork();
+    const nativeToken = useAccountNativeToken(address);
+    const portfolioBalance = nativeToken?.portfolioUSD;
 
-  return (
-    <div
-      className={classNames(
-        "px-4 pt-4 pb-3",
-        "w-[23.25rem] min-w-[23.25rem]",
-        "bg-brand-main/10",
-        "rounded-[.625rem]",
-        "flex flex-col",
-        "relative",
-        className
-      )}
-    >
-      <TippySingletonProvider>
-        <div className="flex">
-          <AutoIcon
-            seed={address}
-            source="dicebear"
-            type="personas"
-            className={classNames(
-              "h-18 w-18 min-w-[4.5rem] mr-4",
-              "bg-black/40",
-              "rounded-[.625rem]"
-            )}
-          />
-          <div
-            className={classNames(
-              "flex flex-col items-start",
-              "text-base text-brand-light leading-none",
-              "w-full min-w-0"
-            )}
+    const transitionRef = useRef<HTMLDivElement>(null);
+
+    return (
+      <div
+        className={classNames(
+          "px-4 pt-4 pb-3",
+          "w-[23.25rem] min-w-[23.25rem]",
+          "bg-brand-main/10",
+          "rounded-[.625rem]",
+          "flex flex-col",
+          "relative overflow-hidden",
+          className
+        )}
+      >
+        <SwitchTransition mode="out-in">
+          <CSSTransition
+            key={address}
+            nodeRef={transitionRef}
+            timeout={{
+              exit: 0,
+              enter: 150,
+            }}
+            classNames={{
+              enter: "opacity-0 translate-x-full",
+              enterActive: "opacity-1 translate-x-0",
+              exit: "opacity-1 duration-0",
+              exitActive: "opacity-0 duration-0",
+            }}
           >
-            <CopiableTooltip
-              content="Copy wallet address to clipboard"
-              textToCopy={address}
-              copiedText="Wallet address copied to clipboard"
-              onCopiedToggle={setCopied}
-              className={classNames(
-                "px-1 pt-1 -mx-1 -mt-1",
-                "text-left",
-                "rounded",
-                "max-w-full",
-                "transition-colors",
-                "hover:bg-brand-main/40"
-              )}
+            <div
+              ref={transitionRef}
+              className={classNames("transform", "transition")}
             >
-              <>
-                <span
-                  className={classNames(
-                    "block",
-                    "pb-0.5 block min-w-0",
-                    "font-bold",
-                    "overflow-ellipsis overflow-hidden whitespace-nowrap"
-                  )}
-                >
-                  <TReplace msg={name} />
-                </span>
-                <span className="inline-flex items-center">
-                  <HashPreview
-                    hash={address}
-                    withTooltip={false}
-                    className="text-sm font-normal leading-none mr-1"
+              <TippySingletonProvider>
+                <div className="flex">
+                  <AutoIcon
+                    seed={address}
+                    source="dicebear"
+                    type="personas"
+                    className={classNames(
+                      "h-18 w-18 min-w-[4.5rem] mr-4",
+                      "bg-black/40",
+                      "rounded-[.625rem]"
+                    )}
                   />
-                  {copied ? (
-                    <SuccessIcon className="w-[1.3125rem] h-auto" />
-                  ) : (
-                    <CopyIcon className="w-[1.3125rem] h-auto" />
+                  <div
+                    className={classNames(
+                      "flex flex-col items-start",
+                      "text-base text-brand-light leading-none",
+                      "w-full min-w-0"
+                    )}
+                  >
+                    <CopiableTooltip
+                      content="Copy wallet address to clipboard"
+                      textToCopy={address}
+                      copiedText="Wallet address copied to clipboard"
+                      onCopiedToggle={setCopied}
+                      className={classNames(
+                        "px-1 pt-1 -mx-1 -mt-1",
+                        "text-left",
+                        "rounded",
+                        "max-w-full",
+                        "transition-colors",
+                        "hover:bg-brand-main/40"
+                      )}
+                    >
+                      <>
+                        <span
+                          className={classNames(
+                            "block",
+                            "pb-0.5 block min-w-0",
+                            "font-bold",
+                            "overflow-ellipsis overflow-hidden whitespace-nowrap"
+                          )}
+                        >
+                          <TReplace msg={name} />
+                        </span>
+                        <span className="inline-flex items-center">
+                          <HashPreview
+                            hash={address}
+                            withTooltip={false}
+                            className="text-sm font-normal leading-none mr-1"
+                          />
+                          {copied ? (
+                            <SuccessIcon className="w-[1.3125rem] h-auto" />
+                          ) : (
+                            <CopyIcon className="w-[1.3125rem] h-auto" />
+                          )}
+                        </span>
+                      </>
+                    </CopiableTooltip>
+                    <PrettyAmount
+                      amount={
+                        nativeToken
+                          ? portfolioBalance ??
+                            ethers.utils.formatEther(nativeToken.rawBalance)
+                          : null
+                      }
+                      currency={portfolioBalance ? "$" : nativeToken?.symbol}
+                      isMinified={
+                        portfolioBalance
+                          ? new BigNumber(portfolioBalance).isLessThan(0.01)
+                          : false
+                      }
+                      copiable
+                      className="mt-auto text-xl font-bold leading-none"
+                    />
+                  </div>
+                </div>
+                <div className="flex mt-2">
+                  <div className="flex justify-center h-6 w-18 min-w-[4.5rem] mr-4">
+                    {currentNetwork?.explorerUrls && (
+                      <IconedButton
+                        href={`${currentNetwork.explorerUrls}/address/${address}`}
+                        aria-label="View wallet in Explorer"
+                        Icon={WalletExplorerIcon}
+                        className="!w-6 !h-6"
+                        iconClassName="!w-[1.125rem]"
+                      />
+                    )}
+                    {currentNetwork?.explorerUrls && (
+                      <IconedButton
+                        href={`${currentNetwork.explorerUrls}/address/${address}`}
+                        aria-label="View wallet activity"
+                        Icon={ClockIcon}
+                        className="!w-6 !h-6 ml-2"
+                        iconClassName="!w-[1.125rem]"
+                      />
+                    )}
+                  </div>
+                  {portfolioBalance && (
+                    <PrettyAmount
+                      amount={
+                        nativeToken
+                          ? ethers.utils.formatEther(nativeToken.rawBalance)
+                          : null
+                      }
+                      currency={nativeToken?.symbol}
+                      copiable
+                      prefix={<GasIcon className="mr-1" />}
+                      className="text-sm leading-none text-brand-inactivedark flex items-center max-h-[1rem]"
+                    />
                   )}
-                </span>
-              </>
-            </CopiableTooltip>
-            <PrettyAmount
-              amount={
-                nativeToken
-                  ? portfolioBalance ??
-                    ethers.utils.formatEther(nativeToken.rawBalance)
-                  : null
-              }
-              currency={portfolioBalance ? "$" : nativeToken?.symbol}
-              isMinified={
-                portfolioBalance
-                  ? new BigNumber(portfolioBalance).isLessThan(0.01)
-                  : false
-              }
-              copiable
-              className="mt-auto text-xl font-bold leading-none"
-            />
-          </div>
-        </div>
-        <div className="flex mt-2">
-          <div className="flex justify-center h-6 w-18 min-w-[4.5rem] mr-4">
-            {currentNetwork?.explorerUrls && (
-              <IconedButton
-                href={`${currentNetwork.explorerUrls}/address/${address}`}
-                aria-label="View wallet in Explorer"
-                Icon={WalletExplorerIcon}
-                className="!w-6 !h-6"
-                iconClassName="!w-[1.125rem]"
-              />
-            )}
-            {currentNetwork?.explorerUrls && (
-              <IconedButton
-                href={`${currentNetwork.explorerUrls}/address/${address}`}
-                aria-label="View wallet activity"
-                Icon={ClockIcon}
-                className="!w-6 !h-6 ml-2"
-                iconClassName="!w-[1.125rem]"
-              />
-            )}
-          </div>
-          {portfolioBalance && (
-            <PrettyAmount
-              amount={
-                nativeToken
-                  ? ethers.utils.formatEther(nativeToken.rawBalance)
-                  : null
-              }
-              currency={nativeToken?.symbol}
-              copiable
-              prefix={<GasIcon className="mr-1" />}
-              className="text-sm leading-none text-brand-inactivedark flex items-center max-h-[1rem]"
-            />
-          )}
-        </div>
-        <Tooltip
-          content={
-            <>
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit ut
-                aliquam, purus sit amet luctus venenatis, lectus magna fringilla
-                urna, porttitor rhoncus dolor purus non enim praesent elementum
-                facilisis leo
-              </p>
-              <p className="mt-2">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit ut
-                aliquam, purus sit amet luctus venenatis, lectus magna fringilla
-                urna, porttitor rhoncus
-              </p>
-            </>
-          }
-          placement="right"
-          size="large"
-          className="absolute right-4 bottom-3"
-        >
-          <TooltipIcon theme="dark" />
-        </Tooltip>
-      </TippySingletonProvider>
-    </div>
-  );
-};
+                </div>
+                <Tooltip
+                  content={
+                    <>
+                      <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit
+                        ut aliquam, purus sit amet luctus venenatis, lectus
+                        magna fringilla urna, porttitor rhoncus dolor purus non
+                        enim praesent elementum facilisis leo
+                      </p>
+                      <p className="mt-2">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit
+                        ut aliquam, purus sit amet luctus venenatis, lectus
+                        magna fringilla urna, porttitor rhoncus
+                      </p>
+                    </>
+                  }
+                  placement="right"
+                  size="large"
+                  className="absolute right-4 bottom-3"
+                >
+                  <TooltipIcon theme="dark" />
+                </Tooltip>
+              </TippySingletonProvider>
+            </div>
+          </CSSTransition>
+        </SwitchTransition>
+      </div>
+    );
+  }
+);
 
 export default LargeWalletCard;
