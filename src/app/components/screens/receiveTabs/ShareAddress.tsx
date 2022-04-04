e44584197a2e23917d7944625749914255355823
Valentyn Shapoval <valentin.vad.sh@gmail.com>
2022-04-04 21:36:20 +0300

    addr

--- src/app/components/screens/receiveTabs/ShareAddress.tsx
+++ src/app/components/screens/receiveTabs/ShareAddress.tsx
@@ -1,22 +1,24 @@
 import { FC, useRef } from "react";
-
-import AddressField from "app/components/elements/AddressField";
+import { useAtomValue } from "jotai";
 import { QRCodeCanvas } from "qrcode.react";
 
+import AddressField from "app/components/elements/AddressField";
+import { currentAccountAtom } from "app/atoms";
+
 const ShareAddress: FC = () => {
-  const value = "0x07a858fc699f99ddF2b186bf162Fd7f4d42F7f63";
+  const currentAccount = useAtomValue(currentAccountAtom);
   const ref = useRef(null);
   return (
     <div className="flex flex-col">
       <AddressField
         ref={ref}
-        defaultValue={value}
+        defaultValue={currentAccount.address}
         label="Wallet address"
         className="mt-5"
         readOnly
       />
       <div className="border-2 border-brand-redtwo shadow-sm m-4 w-fit">
-        <QRCodeCanvas value={value} />
+        <QRCodeCanvas value={currentAccount.address} />
       </div>
     </div>
   );
