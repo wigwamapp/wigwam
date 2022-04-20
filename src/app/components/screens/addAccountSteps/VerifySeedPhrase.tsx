ba5d533a8dff33c394f86fc74778f020d641736b
olehkhalin <olehkhalin@gmail.com>
2022-04-20 11:48:50 -0400

    Edited shuffle function, added autofocus on next input

--- src/app/components/screens/addAccountSteps/VerifySeedPhrase.tsx
+++ src/app/components/screens/addAccountSteps/VerifySeedPhrase.tsx
@@ -21,6 +21,8 @@ import Input from "app/components/elements/Input";
 import AddAccountContinueButton from "app/components/blocks/AddAccountContinueButton";
 import AddAccountHeader from "app/components/blocks/AddAccountHeader";
 
+const WORDS_TO_FILL = 6;
+
 const VerifySeedPhrase = memo(() => {
   const walletStatus = useAtomValue(walletStatusAtom);
   const { alert } = useDialog();
@@ -40,34 +42,10 @@ const VerifySeedPhrase = memo(() => {
     () => (seedPhrase ? fromProtectedString(seedPhrase.phrase).split(" ") : []),
     [seedPhrase]
   );
-  const wordsToCheckPositions = useMemo(() => {
-    const shuffledPositions = shuffle(range(words.length));
-    const selectedPositions: number[] = [];
-    for (let i = 0; i < words.length; i++) {
-      const newPosition = shuffledPositions[i];
-      if (
-        selectedPositions.every((selectedPosition) => {
-          const distance = Math.abs(selectedPosition - newPosition);
-          if (
-            [selectedPosition, newPosition].some((position) =>
-              [0, words.length - 1].some((edge) => edge === position)
-            )
-          ) {
-            return distance > 2;
-          }
-
-          return distance > 2; // was distance > 1
-        })
-      ) {
-        selectedPositions.push(newPosition);
-      }
-      if (selectedPositions.length === WORDS_TO_FILL) {
-        break;
-      }
-    }
-
-    return selectedPositions.sort((a, b) => a - b);
-  }, [words]);
+  const wordsToCheckPositions = useMemo(
+    () => shuffle(range(words.length)),
+    [words]
+  );
 
   const handleContinue = useCallback(async () => {
     try {
@@ -114,7 +92,41 @@ const VerifySeedPhrase = memo(() => {
             className="grid grid-cols-3 gap-y-6 gap-x-9 w-full max-w-[35rem] mx-auto"
           >
             {wordsToCheckPositions.map((indexToFill, i) => (
-              <WordsRow key={i} allWords={words} indexToFill={indexToFill} />
+              <div key={indexToFill} className="flex items-center">
+                <span className="whitespace-nowrap text-base font-bold text-right mr-1.5 block min-w-[1.875rem]">
+                  {indexToFill + 1})
+                </span>
+                <Field
+                  name={`word-${indexToFill}`}
+                  validate={composeValidators(
+                    required,
+                    differentPasswords(words[indexToFill])
+                  )}
+                >
+                  {({ input, meta }) => (
+                    <Input
+                      {...input}
+                      error={meta.touched && meta.error}
+                      success={meta.dirty && !meta.error}
+                      successWithIcon
+                      readOnly={meta.dirty && !meta.error}
+                      inputClassName="max-h-10"
+                      onChange={(evt: any) => {
+                        const { value } = evt.target;
+                        const isError = differentPasswords(value)(
+                          words[indexToFill]
+                        );
+                        if (meta.dirty && !isError) {
+                          if (words[i + 1]) {
+                            focusByName(`word-${wordsToCheckPositions[i + 1]}`);
+                          }
+                        }
+                        input.onChange(evt);
+                      }}
+                    />
+                  )}
+                </Field>
+              </div>
             ))}
             <AddAccountContinueButton loading={submitting} />
           </form>
@@ -126,72 +138,6 @@ const VerifySeedPhrase = memo(() => {
 
 export default VerifySeedPhrase;
 
-type WordsRowProps = {
-  allWords: string[];
-  indexToFill: number;
-};
-
-const WordsRow = memo<WordsRowProps>(({ allWords, indexToFill }) => {
-  const nearIndexes = useMemo(
-    () => getTwoNearIndexes(indexToFill, allWords.length),
-    [indexToFill, allWords.length]
-  );
-  const indexes = useMemo(
-    () => sortNumbers([indexToFill, ...nearIndexes]),
-    [indexToFill, nearIndexes]
-  );
-
-  return (
-    <>
-      {indexes.map((i) => {
-        const toFill = i === indexToFill;
-
-        return (
-          <div key={i} className="flex items-center">
-            <span className="whitespace-nowrap text-base font-bold text-right mr-1.5 block min-w-[1.875rem]">
-              {i + 1})
-            </span>
-            {toFill ? (
-              <Field
-                name={`word-${indexToFill}`}
-                validate={composeValidators(
-                  required,
-                  differentPasswords(allWords[indexToFill])
-                )}
-              >
-                {({ input, meta }) => (
-                  <Input
-                    {...input}
-                    error={meta.touched && meta.error}
-                    success={meta.dirty && !meta.error}
-                    successWithIcon
-                    readOnly={meta.dirty && !meta.error}
-                    inputClassName="max-h-10"
-                  />
-                )}
-              </Field>
-            ) : (
-              <Input
-                readOnly={true}
-                defaultValue={allWords[i]}
-                success={true}
-                successWithIcon
-                inputClassName="max-h-10"
-              />
-            )}
-          </div>
-        );
-      })}
-    </>
-  );
-});
-
-const WORDS_TO_FILL = 2;
-
-const range = (size: number) => {
-  return [...Array(size).keys()].map((i) => i + 0);
-};
-
 const shuffle = (array: any[]) => {
   const length = array == null ? 0 : array.length;
   if (!length) {
@@ -199,29 +145,30 @@ const shuffle = (array: any[]) => {
   }
   let index = -1;
   const lastIndex = length - 1;
-  const result = [...array];
-  while (++index < length) {
+  const result = [];
+  while (++index < WORDS_TO_FILL) {
     const rand = index + Math.floor(Math.random() * (lastIndex - index + 1));
-    const value = result[rand];
-    result[rand] = result[index];
-    result[index] = value;
+    const value = array[rand];
+    array[rand] = array[index];
+    array[index] = value;
+    result.push(value);
   }
-  return result;
+  return sortNumbers(result);
 };
 
-const getTwoNearIndexes = (index: number, limit: number) => {
-  switch (true) {
-    case index === 0:
-      return [1, 2];
-
-    case index === limit - 1:
-      return [limit - 2, limit - 3];
-
-    default:
-      return [index - 1, index + 1];
-  }
+const range = (size: number) => {
+  return [...Array(size).keys()];
 };
 
 const sortNumbers = (arr: number[]) => {
   return arr.sort((a, b) => a - b);
+};
+
+const focusByName = (fieldName: string) => {
+  const elem = document.querySelector(`[name='${fieldName}']`);
+  return (
+    elem &&
+    elem.tagName.toLowerCase() === "input" &&
+    (elem as HTMLInputElement).focus()
+  );
 };
