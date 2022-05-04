2fec21120c8ff90fd7e65bb8381fcb5805c22fc4
olehkhalin <olehkhalin@gmail.com>
2022-05-04 12:36:39 -0400

    Edited popup networks select layout

--- src/app/components/screens/Popup.tsx
+++ src/app/components/screens/Popup.tsx
@@ -54,8 +54,8 @@ const Popup: FC = () => (
     <PreloadUnlocked>
       <NetworkSelect
         className="max-w-auto"
-        currentItemClassName="pr-3 pl-3.5 !py-1.5"
-        currentItemIconClassName="!w-8 !h-8 !mr-3.5"
+        currentItemClassName="pr-3 pl-3 !py-1.5"
+        currentItemIconClassName="!w-8 !h-8 !mr-3"
         contentClassName="w-[22.25rem]"
       />
       <AccountSelect className="mt-2" />
