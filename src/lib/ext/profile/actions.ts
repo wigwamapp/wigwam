0652808a7356dd7c4f63b52edfc4fa90cd8724b2
pas1ko <git@serh11p.com>
2022-07-05 22:19:11 +0100

    Fix window closing after profile changed and only vigvam tab

--- src/lib/ext/profile/actions.ts
+++ src/lib/ext/profile/actions.ts
@@ -5,6 +5,8 @@ import { Profile } from "./types";
 import { loadState, fetchState, setState } from "./state";
 import { generateProfile } from "./helpers";
 
+import { getMainURL } from "../utils";
+
 /**
  * Used to generate keys for other storage entities.
  */
@@ -31,6 +33,16 @@ export async function changeProfile(id: string) {
     currentId: id,
     openTab: true,
   });
+
+  // Open empty tab if there are only one tab and this tab is Vigvam
+  // because after reload this tab will be removed
+  try {
+    const tabs = await browser.tabs.query({ url: getMainURL() });
+
+    if (tabs.length === 1 && tabs[0].index === 0) {
+      await browser.tabs.create({});
+    }
+  } catch {}
 
   browser.runtime.reload();
 }
