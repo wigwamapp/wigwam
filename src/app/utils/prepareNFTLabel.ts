09ce652605eaf00f10f4b9a242975c632a198c2c
olehkhalin <olehkhalin@gmail.com>
2022-08-15 14:53:39 -0400

    - Edited id in name detection - Edited name display in nft info

--- src/app/utils/prepareNFTLabel.ts
+++ src/app/utils/prepareNFTLabel.ts
@@ -1,18 +1,11 @@
 export const prepareNFTLabel = (id: string, name?: string) => {
   const preparedName = name
     ? name.endsWith(id) || name.endsWith(`#${id}`)
-      ? name
-          .replace(` #${id}`, "")
-          .replace(`#${id}`, "")
-          .replace(` ${id}`, "")
-          .replace(id, "")
+      ? name.replace(` #${id}`, "").replace(`#${id}`, "").replace(` ${id}`, "")
       : name
     : undefined;
 
-  const isTrulyId =
-    !preparedName?.includes(id) &&
-    !preparedName?.includes(`#${id}`) &&
-    id.length <= 8;
+  const isTrulyId = !preparedName?.includes(`#${id}`) && id.length <= 8;
 
   return {
     name: preparedName,
