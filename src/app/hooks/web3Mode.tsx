606a595d3750833aad5a81f81dd4323d2af49fd5
pas1ko <git@serh11p.com>
2022-08-19 23:46:12 +0100

    Update content

--- src/app/hooks/web3Mode.tsx
+++ src/app/hooks/web3Mode.tsx
@@ -15,10 +15,11 @@ export function useToggleMetaMaskCompatibleMode() {
         title: "MetaMask compatible mode",
         content: (
           <p className="mb-4">
-            Are you sure you want to disable MetaMask compatible mode? Please
-            note, that you won&apos;t be able to interact with dApps! Also, you
-            will have to refresh active browser tabs where dApps are opened.
-            Learn more about{" "}
+            Are you sure you want to disable MetaMask compatible mode?
+            <br />
+            Please note, that you won&apos;t be able to interact with dApps!
+            Also, you will have to refresh active browser tabs where dApps are
+            opened. Learn more about{" "}
             <a
               href="https://vigvamapp.medium.com/how-vigvam-wallet-may-be-connected-to-any-dapp-with-the-aid-of-metamask-b688f9757184"
               target="_blank"
@@ -41,19 +42,13 @@ export function useToggleMetaMaskCompatibleMode() {
       alert({
         title: "MetaMask compatible mode",
         content: (
-          <p className="mb-4">
-            MetaMask compatible mode enabled. You will have to refresh active
-            browser tabs where dApps are opened. Learn more about{" "}
-            <a
-              href="https://vigvamapp.medium.com/how-vigvam-wallet-may-be-connected-to-any-dapp-with-the-aid-of-metamask-b688f9757184"
-              target="_blank"
-              rel="nofollow noreferrer"
-              className="underline"
-            >
-              how it works here
-            </a>
-            .
-          </p>
+          <>
+            <p>MetaMask compatible mode enabled.</p>
+            <p className="mb-4">
+              You will have to refresh active browser tabs where dApps are
+              opened.
+            </p>
+          </>
         ),
       });
     }
