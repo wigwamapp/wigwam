36f3de4257e99de896f289d7aa3238c72cbf7fed
pas1ko <git@serh11p.com>
2022-10-02 21:47:48 +0100

    Upgrade deps #3: react-number-format

--- src/app/components/elements/NumberInput.tsx
+++ src/app/components/elements/NumberInput.tsx
@@ -1,5 +1,5 @@
 import { forwardRef, memo, useMemo, useRef } from "react";
-import NumberFormat, { NumberFormatProps } from "react-number-format";
+import { NumericFormat, NumericFormatProps } from "react-number-format";
 import { useAtomValue } from "jotai";
 
 import {
@@ -10,7 +10,7 @@ import {
 import { currentLocaleAtom } from "app/atoms";
 import Input, { InputProps } from "./Input";
 
-export type NumberInputProps = Omit<NumberFormatProps, "type"> &
+export type NumberInputProps = Omit<NumericFormatProps, "type"> &
   Omit<InputProps, "ref" | "value" | "defaultValue" | "type" | "onChange"> & {
     defaultValue?: string | number;
     value?: string | number;
@@ -34,18 +34,18 @@ const NumberInput = memo(
       value = value?.toString()?.replace(".", separators.decimals);
 
       return (
-        <NumberFormat
+        <NumericFormat
           getInputRef={ref}
           customInput={Input}
           allowNegative={allowNegative}
           value={value}
           onValueChange={({ value }, { source, event }) => {
-            if (source === "event" && value !== prevValueRef.current) {
+            if (event && source === "event" && value !== prevValueRef.current) {
               prevValueRef.current = value;
               Object.assign(event, {
                 target: { value },
               });
-              onChange?.(event);
+              onChange?.(event as any);
             }
           }}
           {...rest}
