55c564616a24688859ee4235f5aeba8cd5e8cc05
pas1ko <git@serh11p.com>
2023-01-11 21:52:29 +0000

    Add comment

--- public/sw.js
+++ public/sw.js
@@ -1,3 +1,7 @@
+// This file is just a template for `html-webpack-plugin`
+// After build, the scripts that are needed for the extension background script will be added here
+// Used as entry point for MV3 Service Worker
+
 // const importScriptsOriginal = importScripts;
 // globalThis.allScripts = [];
 
