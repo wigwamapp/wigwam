55e9ae5091a89135a0261eab2675997d6f8986c9
pas1ko <git@serh11p.com>
2023-10-18 14:17:11 +0100

    Fix phishing detection, change to blacklist only

--- src/lib/phishing-detect/index.ts
+++ src/lib/phishing-detect/index.ts
@@ -3,9 +3,11 @@ import memoizeOne from "memoize-one";
 import PhishingDetector from "eth-phishing-detect/src/detector";
 import { PHISHING_DETECT_CONFIG } from "fixtures/phishingDetect";
 
-export const isPhishingWebsite = memoize(
-  (hostname: string): boolean => getDetector().check(hostname).result,
-);
+export const isPhishingWebsite = memoize((hostname: string): boolean => {
+  const reason = getDetector().check(hostname);
+
+  return reason.result && reason.type === "blacklist";
+});
 
 const getDetector = memoizeOne(
   () => new PhishingDetector(PHISHING_DETECT_CONFIG),
