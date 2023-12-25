970d43ffce6c641f3491ec32d944b943d9c8adbd
Valerii Shelihan <v.shelihan@wnb.com.ua>
2023-12-25 17:57:47 +0200

    add action option

--- src/app/components/blocks/Sidebar.Links.tsx
+++ src/app/components/blocks/Sidebar.Links.tsx
@@ -1,3 +1,4 @@
+import { useMemo } from "react";
 import { Page } from "app/nav";
 import { ReactComponent as OverviewIcon } from "app/icons/Overview.svg";
 import { ReactComponent as ReceiveIcon } from "app/icons/Receive.svg";
@@ -7,51 +8,81 @@ import { ReactComponent as AppsIcon } from "app/icons/Apps.svg";
 import { ReactComponent as ContactsIcon } from "app/icons/Contacts.svg";
 import { ReactComponent as WalletsIcon } from "app/icons/Wallets.svg";
 import { ReactComponent as SettingsIcon } from "app/icons/Settings.svg";
+import { ReactComponent as SupportIcon } from "app/icons/Support.svg";
+import * as SupportAlert from "app/components/elements/SupportAlert";
+import { useDialog } from "app/hooks/dialog";
 
-export const NavLinksPrimary = [
-  {
-    route: Page.Default,
-    label: "Overview",
-    Icon: OverviewIcon,
-  },
-  {
-    route: Page.Receive,
-    label: "Receive",
-    Icon: ReceiveIcon,
-  },
-  {
-    route: Page.Transfer,
-    label: "Transfer",
-    Icon: SendIcon,
-  },
-  {
-    route: Page.Swap,
-    label: "Swap",
-    Icon: SwapIcon,
-    soon: true,
-  },
-  {
-    route: Page.Apps,
-    label: "Apps",
-    Icon: AppsIcon,
-    soon: true,
-  },
-  {
-    route: Page.Contacts,
-    label: "Contacts",
-    Icon: ContactsIcon,
-  },
-];
+const useSidebarLinks = () => {
+  const { alert } = useDialog();
 
-export const NavLinksSecondary = [
-  {
-    route: Page.Wallets,
-    label: "Wallets",
-    Icon: WalletsIcon,
-  },
-  {
-    route: Page.Settings,
-    label: "Settings",
-    Icon: SettingsIcon,
-  },
-];
+  const NavLinksPrimary = useMemo(() => {
+    return [
+      {
+        route: Page.Default,
+        label: "Overview",
+        Icon: OverviewIcon,
+      },
+      {
+        route: Page.Receive,
+        label: "Receive",
+        Icon: ReceiveIcon,
+      },
+      {
+        route: Page.Transfer,
+        label: "Transfer",
+        Icon: SendIcon,
+      },
+      {
+        route: Page.Swap,
+        label: "Swap",
+        Icon: SwapIcon,
+        soon: true,
+      },
+      {
+        route: Page.Apps,
+        label: "Apps",
+        Icon: AppsIcon,
+        soon: true,
+      },
+      {
+        route: Page.Contacts,
+        label: "Contacts",
+        Icon: ContactsIcon,
+      },
+    ];
+  }, []);
+
+  const NavLinksSecondary = useMemo(() => {
+    return [
+      {
+        route: Page.Wallets,
+        label: "Wallets",
+        Icon: WalletsIcon,
+      },
+      {
+        route: Page.Settings,
+        label: "Settings",
+        Icon: SettingsIcon,
+      },
+      {
+        label: "Support",
+        Icon: SupportIcon,
+        action: () =>
+          alert({
+            title: <SupportAlert.Title />,
+            content: <SupportAlert.Content />,
+          }),
+      },
+    ];
+  }, [alert]);
+
+  return useMemo(
+    () => ({
+      NavLinksPrimary,
+      NavLinksSecondary,
+    }),
+    [NavLinksPrimary, NavLinksSecondary],
+  );
+};
+
+export default useSidebarLinks;
