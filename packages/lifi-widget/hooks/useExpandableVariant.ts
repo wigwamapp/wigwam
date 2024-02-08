627c1bcc47b4405e8308be75dd5f8374270b7698
nektamu <nektamy@gmail.com>
2024-02-08 17:02:16 +0200

    Fix expandable panels

--- packages/lifi-widget/hooks/useExpandableVariant.ts
+++ packages/lifi-widget/hooks/useExpandableVariant.ts
@@ -2,7 +2,7 @@ import type { Theme } from '@mui/material';
 import { useMediaQuery } from '@mui/material';
 import { useWidgetConfig } from '../providers';
 
-const defaultExpandableWidth = 852;
+const defaultExpandableWidth = 100;
 
 export const useExpandableVariant = () => {
   const { variant, useRecommendedRoute } = useWidgetConfig();
