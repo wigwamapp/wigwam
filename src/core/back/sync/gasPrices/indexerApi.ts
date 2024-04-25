7acbec6bc779703fea7102c5dcf0fc64f3bd4c7c
serg-plusplus <gitserg@pm.me>
2024-04-25 13:29:36 +0100

    Improve caching

--- src/core/back/sync/gasPrices/indexerApi.ts
+++ src/core/back/sync/gasPrices/indexerApi.ts
@@ -3,7 +3,11 @@ import { GasPrices } from "core/types";
 import { indexerApi } from "../indexer";
 
 export async function getIndexerGasPrices(chainId: number): Promise<GasPrices> {
-  const { data } = await indexerApi.get<GasPrices>(`/gasprices/${chainId}`);
+  const { data } = await indexerApi.get<GasPrices>(`/gasprices/${chainId}`, {
+    headers: {
+      "Cache-Control": "no-cache",
+    },
+  });
 
   return data;
 }
